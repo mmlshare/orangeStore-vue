144ab3e85124bd10d45aab36b8174e5ca834eb80
mmlshare <mmlshare@163.com>
2020-10-30 17:43:32 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import Routers from './router.js'
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
+
+Vue.use(VueRouter)
+Vue.use(ViewUI)
+
+// The routing configuration
+const RouterConfig = {
+    routes: Routers
+}
+const router = new VueRouter(RouterConfig)
+
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+})
---
+++ src/router.js
@@ -0,0 +1,7 @@
+import Main from './components/Main.vue'
+
+export default [
+    { path: '/', component: Main }
+  ]
+
+
